e52e5c068e36fadb48f4c7f50ca24f82bc49e8bb
Untouchab1e <edvard.holst@gmail.com>
2011-09-05 21:24:16 +0200

    Basic functionality in place along with rough design.

--- Bilskiltoppslag/gen/com/connectutb/bilskilt/R.java
+++ Bilskiltoppslag/gen/com/connectutb/bilskilt/R.java
@@ -11,13 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bgmain=0x7f020000;
+        public static final int bgmainland=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int xfmail=0x7f020003;
+    }
+    public static final class id {
+        public static final int imageButton_Submit=0x7f050001;
+        public static final int regEdit=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        /**  Main Form Strings 
+  Language: Norwegian 
+ Main Form Strings 
+         */
+        public static final int button_submit=0x7f040002;
+        public static final int option_about=0x7f040003;
+        public static final int sms_nr=0x7f040001;
     }
 }
--- Bilskiltoppslag/src/com/connectutb/bilskilt/Main.java
+++ Bilskiltoppslag/src/com/connectutb/bilskilt/Main.java
@@ -1,13 +1,49 @@
 package com.connectutb.bilskilt;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class Main extends Activity {
+	static final private int ABOUT_ID = Menu.FIRST;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /* Set up the listener for the submit button */
+        final ImageButton button = (ImageButton) findViewById(R.id.imageButton_Submit);
+        button.setOnClickListener(new ImageButton.OnClickListener() {
+
+            public void onClick(View v) {
+                // Run the sendRequest method 
+            	sendRequest();
+            }
+        });
+    }
+    
+    public void sendRequest(){
+    	/* Send the license info on SMS */
+    		final EditText regfield = (EditText) findViewById(R.id.regEdit);
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(String.valueOf(R.string.sms_nr), null, regfield.getText().toString(), null, null); 
+
+    }
+    
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /**
+         * Create our menu options
+         */
+    	super.onCreateOptionsMenu(menu);
+    	
+    	menu.add(0, ABOUT_ID, 0, R.string.option_about).setShortcut('0', 'o').setIcon(android.R.drawable.ic_dialog_info);
+    	return true;
     }
 }
